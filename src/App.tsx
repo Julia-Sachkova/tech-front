82729fe039e596f3c07c57b37671abf95c1a6352
Julia-Rulova <yu.rylova@itstaffer.ru>
2024-07-18 13:06:24 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@mui/material";
+
+function App() {
+  return (
+    <div className="bg-black">
+      <Button variant="contained">some button</Button>
+    </div>
+  );
+}
+
+export default App;
