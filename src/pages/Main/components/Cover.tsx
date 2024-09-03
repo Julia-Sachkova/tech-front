6ccc6134949a21347488044cfca0d43361d05ee2
Julia-Sachkova <julia.rulova0066@gmail.com>
2024-09-03 15:23:50 +0300

    cover new

--- src/pages/Main/components/Cover.tsx
+++ src/pages/Main/components/Cover.tsx
@@ -1,6 +1,7 @@
 import { Icon } from "@iconify/react";
 import { Link } from "react-router-dom";
 import mainPhoto from "../../../assets/images/mainPhoto.png";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 //import { Rating } from "@mui/material";
 
 const Cover = () => {
@@ -34,6 +35,23 @@ const Cover = () => {
           </li>
         </ul>
 
+        <div className="mt-12 flex flex-col gap-20">
+          <ul className="list-disc flex flex-col gap-1 pl-6 text-xl">
+            <li>Получишь опыт приближенный к коммерческому</li>
+            <li>Получишь опыт разработки фулстак приложения с нуля</li>
+            <li>Получишь пет-проект для портфолио</li>
+            <li>Освоишь сложные принципы и концепции разаботки</li>
+          </ul>
+
+          <Link
+            to="/"
+            className="flex flex-row items-center gap-4 py-3 px-5 bg-primary rounded-full w-fit transition ease-in-out duration-300 hover:scale-110"
+          >
+            <span className="text-2xl">БЕСПЛАТНАЯ КОНСУЛЬТАЦИЯ</span>
+            <ArrowForwardIcon fontSize="large" />
+          </Link>
+        </div>
+
         {/* <div className="flex flex-row gap-5 relative px-3 py-2 mt-auto mb-6 w-fit">
           <div className="absolute top-0 right-0 bottom-0 left-0 bg-primary opacity-20 rounded-lg" />
           <span className="text-5xl font-semibold z-10">9.5</span>
@@ -52,28 +70,28 @@ const Cover = () => {
         src={mainPhoto}
       />
 
-      <span className="text-neutral-600 text-4xl font-semibold -rotate-45 absolute top-1/3 right-[45%] z-10 animate-opacityHidden hidden lg:block">
+      <span className="text-neutral-600 text-4xl font-semibold -rotate-45 absolute top-1/4 right-[45%] z-10 animate-opacityHidden hidden lg:block">
         Web
       </span>
-      <span className="text-neutral-600 text-3xl font-semibold rotate-12 absolute top-10 right-[48%] z-10 animate-opacityHidden hidden lg:block">
+      <span className="text-neutral-600 text-3xl font-semibold rotate-12 absolute top-[30%] right-[55%] z-10 animate-opacityHidden hidden lg:block">
         EF Core
       </span>
       <span className="text-neutral-600 text-3xl font-semibold rotate-12 absolute top-[43%] right-[57%] z-10 animate-opacityHidden hidden lg:block">
         CSS
       </span>
-      <span className="text-neutral-600 text-6xl font-semibold absolute top-1/2 right-[40%] z-10 animate-opacityHidden hidden lg:block">
+      <span className="text-neutral-600 text-6xl font-semibold absolute bottom-[5%] left-[5%] z-10 animate-opacityHidden hidden lg:block">
         C#
       </span>
-      <span className="text-neutral-600 text-5xl font-semibold absolute top-32 right-[52%] z-10 animate-opacityHidden hidden lg:block">
+      <span className="text-neutral-600 text-5xl font-semibold absolute top-[17%] right-[52%] z-10 animate-opacityHidden hidden lg:block">
         React
       </span>
-      <span className="text-neutral-600 text-6xl font-semibold -rotate-12 absolute top-20 right-1/3 z-10 animate-opacityHidden hidden lg:block">
+      <span className="text-neutral-600 text-6xl font-semibold -rotate-12 absolute top-[5%] right-1/3 z-10 animate-opacityHidden hidden lg:block">
         .Net
       </span>
-      <span className="text-neutral-600 text-4xl font-semibold rotate-12 absolute bottom-20 right-[40%] z-10 animate-opacityHidden hidden lg:block">
+      <span className="text-neutral-600 text-4xl font-semibold rotate-6 absolute bottom-[20%] left-[15%] z-10 animate-opacityHidden hidden lg:block">
         TypeScript
       </span>
-      <span className="text-neutral-600 text-3xl font-semibold -rotate-12 absolute bottom-16 right-[58%] z-10 animate-opacityHidden hidden lg:block">
+      <span className="text-neutral-600 text-3xl font-semibold -rotate-12 absolute bottom-[8%] right-[58%] z-10 animate-opacityHidden hidden lg:block">
         Docker
       </span>
       <span className="text-neutral-600 text-4xl font-semibold -rotate-12 absolute bottom-1/3 right-1/2 z-10 animate-opacityHidden hidden lg:block">
